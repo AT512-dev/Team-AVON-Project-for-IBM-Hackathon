ed8faaf64c8948edaa5a66dcf28a7d50becaf540
ALEKS ALEKS <aleksandre2030@gmail.com>
2026-05-01 18:27:39 +0400

    Delete start.bat

--- start.bat
+++
@@ -1,8 +0,0 @@
-# 1. Pull the remote changes and allow unrelated histories
-git pull origin main --allow-unrelated-histories
-
-# 2. Stage everything again just in case
-git add .
-
-# 3. Push it up
-git push -u origin main
